13df530c9519466e1226bd3f6be464b31c3ff698
wizardwhosaysni <the_wizard_who_says_ni@hotmail.com>
2015-08-21 00:18:50 +0200

    Added bit-perfect check after build

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,15 @@
 echo off
 cls
+
+echo -------------------------------------------------------------
 echo Start of assembly
 cd ../disasm/
+SET "buildname=sf1build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin"
 echo Executing assembler ...
-@"../tools/asm68k" /o ae- /p sf1.asm, "../build/sf1build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin" > ../build/output.log
-echo End of assembly, produced sf1build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin
+@"../tools/asm68k" /o ae- /p sf1.asm, "../build/%buildname%" > ../build/output.log
+echo End of assembly, produced %buildname%
+cd ../build/
+echo -------------------------------------------------------------
+IF EXIST "%buildname%" (IF EXIST sf1.bin (fc /b "%buildname%" sf1.bin) ELSE echo wut) ELSE echo "%buildname%" does not exist, probably due to an assembly error. Check output.log.
+
 pause
