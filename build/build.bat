15b2c3aa40f71c98d2df8ad88b34967cb8d642b4
xenometal <2898219+xenometal@users.noreply.github.com>
2018-05-16 15:06:58 -0400

    Update build.bat to be locale-agnostic

--- build/build.bat
+++ build/build.bat
@@ -1,10 +1,22 @@
 echo off
 cls
-
+for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+set month=0%Month%
+set month=%month:~-2%
+set day=0%Day%
+set day=%day:~-2%
+set today=%Year%%month%%day%
+set hour=%Hour%
+set hour=0%hour%
+set hour=%hour:~-2%
+set minutes=0%Minute%
+set minutes=%minutes:~-2%
+set seconds=0%Second%
+set seconds=%seconds:~-2%
 echo -------------------------------------------------------------
 echo Start of assembly
 cd ../disasm/
-SET "buildname=sf1build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin"
+SET "buildname=sf1build-%today%-%hour%%minutes%%seconds%.bin"
 echo Executing assembler ...
 @"../tools/asm68k" /o ae- /p sf1.asm, "../build/%buildname%" > ../build/output.log
 echo End of assembly, produced %buildname%
