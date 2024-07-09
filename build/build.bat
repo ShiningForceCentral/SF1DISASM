05d920bfba1f146968b05c56eff947971fb64b59
xenometal <2898219+xenometal@users.noreply.github.com>
2024-07-09 00:49:08 -0400

    Improve build script : - Produce listings and symbols files - Automatically fix ROM header checksum

--- build/build.bat
+++ build/build.bat
@@ -27,13 +27,16 @@ cd ../musicbank1/
 ..\..\..\..\tools\asw\p2bin.exe .\musicbank1.p ..\musicbank1.bin -k -r $8ae0-$ffff
 cd ../../../
 echo Assembling game ...
-SET "buildname=sf1build-%today%-%hour%%minutes%%seconds%.bin"
-@"../tools/asm68k" /o ae- /p sf1.asm, "../build/%buildname%" > ../build/output.log
-echo End of assembly, produced %buildname%
+SET "buildname=sf1build-%today%-%hour%%minutes%%seconds%"
+@"../tools/asm68k" /k /m /o ae- /p sf1.asm, "../build/%buildname%.bin", ../build/%buildname%.sym, ../build/%buildname%.lst > ../build/output.log
+echo End of assembly, produced %buildname%.bin
 cd ../build/
 
 echo -------------------------------------------------------------
 echo Checking build ...
-IF EXIST "%buildname%" (IF EXIST ../rom/sf1.bin (fc /b "%buildname%" ../rom/sf1.bin) ELSE echo sf1.bin does not exist in rom directory) ELSE echo "%buildname%" does not exist, probably due to an assembly error. Check output.log.
+IF EXIST "%buildname%.bin" ..\tools\fixheader "%buildname%.bin"
+IF EXIST "%buildname%.bin" copy "%buildname%.bin" sf1build-last.bin
+IF EXIST "%buildname%.bin" copy "%buildname%.lst" sf1build-last.lst
+IF EXIST "%buildname%.bin" (IF EXIST ../rom/sf1.bin (fc /b "%buildname%.bin" ../rom/sf1.bin) ELSE echo sf1.bin does not exist in rom directory) ELSE echo "%buildname%.bin" does not exist, probably due to an assembly error. Check output.log.
 
 pause
