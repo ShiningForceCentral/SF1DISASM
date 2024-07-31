99d7be50106c082fd9688cbbd92cc4cb63651e81
Accumulus41 <88561498+Accumulus41@users.noreply.github.com>
2024-07-31 18:27:50 -0500

    Create shopdata.asm

---
+++ disasm/data/stats/items/shopdata.asm
@@ -0,0 +1,203 @@
+
+; ASM FILE data\stats\items\shopdata.asm :
+; 0x16F62..0x17028 : Shop data
+
+rpt_ShopInventories:
+		dc.w ShopInventory00-ShopInventory00
+		dc.w ShopInventory01-ShopInventory00
+		dc.w ShopInventory02-ShopInventory00
+		dc.w ShopInventory03-ShopInventory00
+		dc.w ShopInventory04-ShopInventory00
+		dc.w ShopInventory05-ShopInventory00
+		dc.w ShopInventory06-ShopInventory00
+		dc.w ShopInventory07-ShopInventory00
+		dc.w ShopInventory08-ShopInventory00
+		dc.w ShopInventory09-ShopInventory00
+		dc.w ShopInventory10-ShopInventory00
+		dc.w ShopInventory11-ShopInventory00
+		dc.w ShopInventory12-ShopInventory00
+		dc.w ShopInventory13-ShopInventory00
+		dc.w ShopInventory14-ShopInventory00
+		dc.w ShopInventory15-ShopInventory00
+		dc.w ShopInventory16-ShopInventory00
+		dc.w ShopInventory17-ShopInventory00
+		dc.w ShopInventory18-ShopInventory00
+		dc.w ShopInventory19-ShopInventory00
+		dc.w ShopInventory20-ShopInventory00
+		dc.w ShopInventory21-ShopInventory00
+		dc.w ShopInventory22-ShopInventory00
+		dc.w ShopInventory23-ShopInventory00
+                
+ShopInventory00:
+    shopList WEAPON, &
+        SHORT_SWORD, &
+        SPEAR, &
+        HAND_AXE, &
+        WOODEN_STAFF, &
+        WOODEN_ARROW
+                
+ShopInventory01:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANGEL_WING
+                
+ShopInventory02:
+    shopList WEAPON, &
+        SHORT_SWORD, &
+        SPEAR, &
+        HAND_AXE, &
+        WOODEN_STAFF, &
+        WOODEN_ARROW
+                
+ShopInventory03:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANGEL_WING
+                
+ShopInventory04
+    shopList WEAPON, &
+        MIDDLE_SWORD, &
+        SPEAR, &
+        BRONZE_LANCE, &
+        POWER_STAFF, &
+        WOODEN_STAFF, &
+        WOODEN_ARROW
+                
+ShopInventory05:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory06:
+    shopList WEAPON
+                
+ShopInventory07:
+    shopList ITEM
+                
+ShopInventory08:
+    shopList WEAPON, &
+        MIDDLE_SWORD, &
+        POWER_SPEAR, &
+        BRONZE_LANCE, &
+        MIDDLE_AXE, &
+        POWER_STAFF, &
+        STEEL_ARROW
+                
+ShopInventory09:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory10:
+    shopList WEAPON, &
+        LONG_SWORD, &
+        POWER_SPEAR, &
+        BRONZE_LANCE, &
+        MIDDLE_AXE, &
+        POWER_STAFF, &
+        STEEL_ARROW
+                
+ShopInventory11:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory12:
+    shopList WEAPON, &
+        LONG_SWORD, &
+        POWER_SPEAR, &
+        BRONZE_LANCE, &
+        BATTLE_AXE, &
+        POWER_STAFF, &
+        STEEL_ARROW
+                
+ShopInventory13:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory14:
+    shopList WEAPON, &
+        STEEL_SWORD, &
+        POWER_SPEAR, &
+        STEEL_LANCE, &
+        BATTLE_AXE, &
+        POWER_STAFF, &
+        ELVEN_ARROW
+                
+ShopInventory15:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory16:
+    shopList WEAPON, &
+        STEEL_SWORD, &
+        CHROME_LANCE, &
+        BATTLE_AXE, &
+        GUARDIAN_STAFF, &
+        ELVEN_ARROW
+                
+ShopInventory17:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory18:
+    shopList WEAPON, &
+        STEEL_SWORD, &
+        CHROME_LANCE, &
+        BATTLE_AXE, &
+        GUARDIAN_STAFF, &
+        ASSAULT_SHELL
+                
+ShopInventory19:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory20:
+    shopList WEAPON, &
+        BROAD_SWORD, &
+        CHROME_LANCE, &
+        GREAT_AXE, &
+        HOLY_STAFF, &
+        ASSAULT_SHELL
+                
+ShopInventory21:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
+                
+ShopInventory22:
+    shopList WEAPON, &
+        BROAD_SWORD, &
+        CHROME_LANCE, &
+        GREAT_AXE, &
+        HOLY_STAFF, &
+        BUSTER_SHOT
+                
+ShopInventory23:
+    shopList ITEM, &
+        MEDICAL_HERB, &
+        HEALING_SEED, &
+        ANTIDOTE, &
+        ANGEL_WING
