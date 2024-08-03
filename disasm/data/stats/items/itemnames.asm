74be71d78cc9820de6049d7710b6dab1d870f65a
Accumulus41 <88561498+Accumulus41@users.noreply.github.com>
2024-08-03 15:42:02 -0500

    Create itemnames.asm

---
+++ disasm/data/stats/items/itemnames.asm
@@ -0,0 +1,70 @@
+
+; ASM FILE data\stats\items\itemnames.asm :
+; 0x25640..0x25916 : Item Names
+
+ItemNames:
+
+    itemName "Medical", 9, "Herb"   ; MEDICAL_HERB
+    itemName "Healing", 9, "Seed"   ; HEALING_SEED
+    itemName "Antidote"   ; ANTIDOTE
+    itemName "Shower", 9, "of Cure"   ; SHOWER_OF_CURE
+    itemName "Angel", 9, "Wing"   ; ANGEL_WING
+    itemName "Power", 9, "Potion"   ; POWER_POTION
+    itemName "Defense", 9, "Potion"   ; DEFENSE_POTION
+    itemName "Legs of", 9, "Haste"   ; LEGS_OF_HASTE
+    itemName "Turbo", 9, "Pepper"   ; TURBO_PEPPER
+    itemName "Bread", 9, "of Life"   ; BREAD_OF_LIFE
+    itemName "Power", 9, "Ring"   ; POWER_RING
+    itemName "Shield", 9, "Ring"   ; SHIELD_RING
+    itemName "Speed", 9, "Ring"   ; SPEED_RING
+    itemName "Mobility", 9, "Ring"   ; MOBILITY_RING
+    itemName "White", 9, "Ring"   ; WHITE_RING
+    itemName "Black", 9, "Ring"   ; BLACK_RING
+    itemName "Evil", 9, "Ring"   ; EVIL_RING
+    itemName "Sugoi", 9, "Mizugi"   ; SUGOI_MIZUGI
+    itemName "Orb of", 9, "Light"   ; ORB_OF_LIGHT
+    itemName "Moon", 9, "Stone"   ; MOON_STONE
+    itemName "Lunar", 9, "Dew"   ; LUNAR_DEW
+    itemName "Kitui", 9, "Huku"   ; KITUI_HUKU
+    itemName "Domingo", 9, "Egg"   ; DOMINGO_EGG
+    itemName "Kenji"   ; KENJI
+    itemName "Teppou"   ; TEPPOU
+    itemName "Kaku-", 9, "chan"   ; KAKU_CHAN
+    itemName "Youji"   ; YOUJI
+    itemName "Great", 9, "Axe"   ; GREAT_AXE
+    itemName "Kindan", 9, "NoHako"   ; KINDAN_NO_HAKO
+    itemName "Short", 9, "Sword"   ; SHORT_SWORD
+    itemName "Middle", 9, "Sword"   ; MIDDLE_SWORD
+    itemName "Long", 9, "Sword"   ; LONG_SWORD
+    itemName "Steel", 9, "Sword"   ; STEEL_SWORD
+    itemName "Broad", 9, "Sword"   ; BROAD_SWORD
+    itemName "Doom", 9, "Blade"   ; DOOM_BLADE
+    itemName "Katana"   ; KATANA
+    itemName "Elven", 9, "Arrow"   ; ELVEN_ARROW
+    itemName "Sword of", 9, "Darkness"   ; SWORD_OF_DARKNESS
+    itemName "Sword of", 9, "Light"   ; SWORD_OF_LIGHT
+    itemName "Chaos", 9, "Breaker"   ; CHAOS_BREAKER
+    itemName "Bronze", 9, "Lance"   ; BRONZE_LANCE
+    itemName "Steel", 9, "Lance"   ; STEEL_LANCE
+    itemName "Chrome", 9, "Lance"   ; CHROME_LANCE
+    itemName "Devil", 9, "Lance"   ; DEVIL_LANCE
+    itemName "Halberd"   ; HALERD
+    itemName "Spear"   ; SPEAR
+    itemName "Power", 9, "Spear"   ; POWER_SPEAR
+    itemName "Valkyrie"   ; VALKYRIE
+    itemName "Hand Axe"   ; HAND_AXE
+    itemName "Middle", 9, "Axe"   ; MIDDLE_AXE
+    itemName "Battle", 9, "Axe"   ; BATTLE_AXE
+    itemName "Heat Axe"   ; HEAT_AXE
+    itemName "Atlas"   ; ATLAS
+    itemName "Wooden", 9, "Staff"   ; WOODEN_STAFF
+    itemName "Guardian", 9, "Staff"   ; GUARDIAN_STAFF
+    itemName "Holy", 9, "Staff"   ; HOLY_STAFF
+    itemName "Power", 9, "Staff"   ; POWER_STAFF
+    itemName "Demon", 9, "Rod"   ; DEMON_ROD
+    itemName "Yogurt", 9, "Ring"   ; YOGURT_RING
+    itemName "Wooden", 9, "Arrow"   ; WOODEN_ARROW
+    itemName "Steel", 9, "Arrow"   ; STEEL_ARROW
+    itemName "Assault", 9, "Shell"   ; ASSAULT_SHELL
+    itemName "Buster", 9, "Shot"   ; BUSTER_SHOT
+
