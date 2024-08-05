2288b7cc62029d39c568bb1d84ad9419ad2459b7
Accumulus41 <88561498+Accumulus41@users.noreply.github.com>
2024-08-05 12:25:21 -0500

    Create spellnames.asm

---
+++ disasm/data/stats/spells/spellnames.asm
@@ -0,0 +1,24 @@
+
+; ASM FILE data\stats\spells\spellnames.asm :
+; 0x26404..0x26466 : Spell names
+
+SpellNames:
+
+    spellName "HEAL"
+    spellName "AURA"
+    spellName "DETOX"
+    spellName "QUICK"
+    spellName "SLOW"
+    spellName "BOOST"
+    spellName "DISPEL"
+    spellName "SHIELD"
+    spellName "MUDDLE"
+    spellName "BLAZE"
+    spellName "FREEZE"
+    spellName "BOLT"
+    spellName "DESOUL"
+    spellName "EGRESS"
+    spellName "dummy"
+    spellName "SLEEP"
+    
+    
