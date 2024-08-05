5d094d0e619cf09fd952f6cf4960ddedf168bb00
Accumulus41 <88561498+Accumulus41@users.noreply.github.com>
2024-08-05 13:12:13 -0500

    Create spelldata.asm

---
+++ disasm/data/stats/spells/spelldata.asm
@@ -0,0 +1,263 @@
+
+; ASM FILE data\stats\spells\spelldata.asm :
+; 0x26466..0x26566 : Spell definitions
+
+SpellData:
+
+    ; HEAL 1
+    spellEffect HEAL1, HEAL_1
+    mpCost      3
+    
+    ; HEAL 2
+    spellEffect HEAL_2, HEAL_2
+    mpCost      5
+    
+    ; HEAL 3
+    spellEffect HEAL_3, HEAL_3
+    mpCost      10
+    
+    ; HEAL 4
+    spellEffect HEAL_4, HEAL_4
+    mpCost      20
+    
+    ; AURA 1
+    spellEffect AURA_1, AURA_1
+    mpCost      7
+    
+    ; AURA 2
+    spellEffect AURA_2, AURA_2
+    mpCost      11
+    
+    ; AURA 3
+    spellEffect AURA_3, AURA_3
+    mpCost      15
+    
+    ; AURA 4
+    spellEffect AURA_4, AURA_4
+    mpCost      18
+    
+    ; DETOX 1
+    spellEffect DETOX_1, DETOX_1
+    mpCost      3
+    
+    ; DETOX 2
+    spellEffect DETOX_2, DETOX_2
+    mpCost      6
+    
+    ; DETOX 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DETOX 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; QUICK 1
+    spellEffect QUICK_1, QUICK
+    mpCost      5
+    
+    ; QUICK 2
+    spellEffect QUICK_2, QUICK
+    mpCost      16
+    
+    ; QUICK 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; QUICK 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SLOW 1
+    spellEffect SLOW_1, SLOW
+    mpCost      5
+    
+    ; SLOW 2
+    spellEffect SLOW_2, SLOW
+    mpCost      20
+    
+    ; SLOW 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SLOW 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; BOOST 1
+    spellEffect BOOST, BOOST
+    mpCost      15
+    
+    ; BOOST 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; BOOST 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; BOOST 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DISPEL 1
+    spellEffect DISPEL, DISPEL
+    mpCost      5
+    
+    ; DISPEL 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DISPEL 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DISPEL 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SHIELD 1
+    spellEffect SHIELD, SHIELD
+    mpCost      5
+    
+    ; SHIELD 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SHIELD 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SHIELD 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; MUDDLE 1
+    spellEffect MUDDLE, MUDDLE
+    mpCost      6
+    
+    ; MUDDLE 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; MUDDLE 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; MUDDLE 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; BLAZE 1
+    spellEffect BLAZE_1, BLAZE_1
+    mpCost      2
+    
+    ; BLAZE 2
+    spellEffect BLAZE_2, BLAZE_2
+    mpCost      5
+    
+    ; BLAZE 3
+    spellEffect BLAZE_3, BLAZE_3
+    mpCost      8
+    
+    ; BLAZE 4
+    spellEffect BLAZE_4, BLAZE_4
+    mpCost      8
+    
+    ; FREEZE 1
+    spellEffect FREEZE_1, FREEZE_1
+    mpCost      3
+    
+    ; FREEZE 2
+    spellEffect FREEZE_2, FREEZE_2
+    mpCost      7
+    
+    ; FREEZE 3
+    spellEffect FREEZE_3, FREEZE_3
+    mpCost      10
+    
+    ; FREEZE 4
+    spellEffect FREEZE_4, FREEZE_4
+    mpCost      10
+    
+    ; BOLT 1
+    spellEffect BOLT_1, BOLT_1
+    mpCost      8
+    
+    ; BOLT 2
+    spellEffect BOLT_2, BOLT_2
+    mpCost      15
+    
+    ; BOLT 3
+    spellEffect BOLT_3, BOLT_3
+    mpCost      20
+    
+    ; BOLT 4
+    spellEffect BOLT_4, BOLT_4
+    mpCost      20
+    
+    ; DESOUL 1
+    spellEffect DESOUL_1, DESOUL_1
+    mpCost      8
+    
+    ; DESOUL 2
+    spellEffect DESOUL_2, DESOUL_2
+    mpCost      15
+    
+    ; DESOUL 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DESOUL 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; EGRESS 1
+    spellEffect EGRESS, EGRESS
+    mpCost      8
+    
+    ; EGRESS 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; EGRESS 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; EGRESS 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DUMMY 1
+    spellEffect SLEEP, SLEEP
+    mpCost      20
+    
+    ; DUMMY 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DUMMY 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; DUMMY 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SLEEP 1
+    spellEffect SLEEP, SLEEP
+    mpCost      6
+    
+    ; SLEEP 2
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SLEEP 3
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    ; SLEEP 4
+    spellEffect 0, ATTACK
+    mpCost      0
+    
+    
